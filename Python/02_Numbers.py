374867b5c7b53410d85c44f49d3b370cd1e89ae0
Thiru A <thirunavukkarasu143@gmail.com>
2021-01-04 08:22:39 +0530

    Final commit on Numbers

--- Python/02_Numbers.py
+++ Python/02_Numbers.py
@@ -12,6 +12,8 @@ def arithmatic_operation(num1, num2,symbol):
         print("Divison of {} and {} : {}".format(num1, num2, (num1/num2)))
     elif symbol == "%":
         print("Mod of {} and {} : {}".format(num1, num2, (num1 % num2)))
+    elif symbol == "**":
+        print("Square root of {} of {} : {}".format(num1, num2, (num1**num2)))
     else:
         print("Invalid Operation")
 
@@ -23,3 +25,17 @@ arithmatic_operation(1, 10, '-')
 arithmatic_operation(5, 8, '*')
 arithmatic_operation(8, 2, '/')
 arithmatic_operation(6, 4, '%')
+arithmatic_operation(2, 3, '**')
+
+# Find the type of the variable.
+
+
+a = 1
+print("Type of a: " + str(type(a)))
+
+b = 1.2
+print('Type of b: ' + str(type(b)))
+
+c = 0o101
+print('Type of c: ' + str(type(c)))
+
